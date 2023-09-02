16ce8f57a94e600a859685387b8f81dab6c547a6
scamilcar <raclimacs@gmail.com>
2023-09-02 13:09:55 +0200

    deposit tests

--- test/live/boosters/Deposit.t.sol
+++ test/live/boosters/Deposit.t.sol
@@ -5,21 +5,28 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 import {BaseTest} from "test/live/BaseTest.sol";
 
+import "forge-std/console.sol";
+
 // vm.expectRevert(stdError.arithmeticError)
+// vm.expectRevert(contract.Error.selector)
 
 contract BoosterDepositTest is BaseTest {
     function setUp() public {
         deploy();
     }
 
-    function test_deposit() public  returns(bool) {
-        // assume
-        // vm.assume(assets > 1e6);
-        // vm.assume(assets < IERC20(poolPosition).balanceOf(alice));
+    function test_deposit(uint256 assets) public  returns(bool) {
+        // bound
+        assets = bound(assets, 1, IERC20(poolPosition).balanceOf(alice));
+
+        address recipient = alice;
+        
         // deposit
         vm.startPrank(alice);
-        IERC20(poolPosition).approve(address(booster), 1e18);
-        booster.deposit(1e18, alice);
+        IERC20(poolPosition).approve(address(booster), assets);
+        booster.deposit(assets, alice);
         vm.stopPrank();
+
+        // assertEq()
     }
 }
