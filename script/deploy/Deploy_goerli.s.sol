25580e07e66172f799c3fbe4513e2c55b9e2edb6
scamilcar <raclimacs@gmail.com>
2023-09-05 14:11:29 +0200

    script for prod

--- script/deploy/Deploy_goerli.s.sol
+++ script/deploy/Deploy_goerli.s.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import {MockLocker} from "../utils/MockLocker.sol";
+// import {MockLocker} from "../utils/MockLocker.sol";
+
+import {Locker} from "contracts/core/Locker.sol";
+import {MockERC20} from "@solmate/test/utils/mocks/MockERC20.sol";
 
 contract Deploy_goerli is Script {
 
-    address mav = address(1);
+    address mav;
     address endpoint = 0xbfD2135BFfbb0B5378b56643c2Df8a87552Bfa23;
 
     function run() external {
@@ -14,9 +17,9 @@ contract Deploy_goerli is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        new MockLocker(mav, endpoint);
+        mav = address(new MockERC20("Maverick", "MAV", 18));
+        new Locker(mav, endpoint);
 
         vm.stopBroadcast();
     }
-
 }
