1e8bc0b5c737a1f42e27cbe2abd558a88211c90f
scamilcar <raclimacs@gmail.com>
2023-10-02 13:45:16 +0200

    adapt locker

--- contracts/core/Locker.sol
+++ contracts/core/Locker.sol
@@ -1,45 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
-
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {OFT} from "@layerzerolabs/solidity-examples/contracts/token/oft/OFT.sol";
-
 import {IBoard} from "contracts/periphery/interfaces/IBoard.sol";
 
 contract Locker is OFT {
-    using SafeERC20 for IERC20;
-
     error ZeroAmount();
     error NoDeposit();
-    error LowBalance();
     error BoardAlreadySet();
-    error BoardNotSet();
     error Disabled();
     error NotDisabled();
     error InvalidIncentiveValue(uint256 incentive);
     error InvalidBoard();
+    error TimelockPeriodNotPassed();
+    error InvalidDisabling();
 
-    event BoardSet(address board);
+    event BoardSet(address board, uint256 incentive);
     event IncentiveUpdated(uint256 incentive);
     event WithdrawalsDisabled();
 
-    /// @notice address of MAV token
+    /// @notice address of the MAV token
     IERC20 public immutable mav;
-
-    /// @notice amount of MAV locked on Rogue
-    uint256 public totalLocked;
 
     /// @notice address of the Board contract
     address public board;
 
-    /// @notice current incentive to call lock
+    /// @notice current incentive to extend the lock
     uint256 public callIncentive;
 
     /// @notice returns true if withdrawals are disabled
     bool public disabled;
+
+    /// @notice timestamp at which Board was set
+    uint256 public boardSetAt;
 
     /// @notice 100%
     uint256 public constant ONE = 1e18;
@@ -47,8 +41,8 @@ contract Locker is OFT {
     /// @notice 1%
     uint256 public constant maxIncentive = 0.01e18;
 
-    /// @param _mav address of MAV contract
-    /// @param _lzEndPoint address of LayerZero endpoint
+    /// @param _mav address of the MAV contract
+    /// @param _lzEndPoint address of the LayerZero endpoint
     constructor(address _mav, address _lzEndPoint) OFT("Rogue MAV", "rMAV", _lzEndPoint) {
         mav = IERC20(_mav);
     }
@@ -57,55 +51,51 @@ contract Locker is OFT {
     ////////////////////////// User Facing /////////////////////////
     ////////////////////////////////////////////////////////////////
 
-    /// @notice deposit MAV and mint rMAV
-    /// @param amount amount of MAV to lock pulled from `msg.sender`
+    /// @notice pulls MAV from caller and mint rMAV to `recipient`
+    /// @param amount amount of MAV to deposit
     /// @param recipient address to mint rMAV to
     function deposit(uint256 amount, address recipient) external {
         if (amount == 0) revert ZeroAmount();
-        mav.safeTransferFrom(msg.sender, address(this), amount);
-        totalLocked += amount;
+        mav.transferFrom(msg.sender, address(this), amount);
         _mint(recipient, amount);
     }
 
-    /// @notice withdraw MAV and burn rMAV
-    /// @param amount amount of rMAV to withdraw
+    /// @notice withdraws MAV and burns rMAV
+    /// @param amount amount of MAV to withdraw
     function withdraw(uint256 amount) external {
         if (disabled) revert Disabled();
         if (amount == 0) revert ZeroAmount();
-        if (balanceOf(msg.sender) < amount) revert LowBalance();
         _burn(msg.sender, amount);
-        totalLocked -= amount;
-        mav.safeTransfer(msg.sender, amount);
+        mav.transfer(msg.sender, amount);
     }
 
-    /// @notice extend locks, caller get rMAV minted as incentive
+    /// @notice extends the lock on the board, incentivized call
     function lock() external {
         if (!disabled) revert NotDisabled();
-        if (board == address(0)) revert BoardNotSet();
         uint256 balance = mav.balanceOf(address(this));
         if (balance == 0) revert NoDeposit();
-        uint256 incentive = Math.mulDiv(balance, callIncentive, ONE);
-        IBoard(board).extendLockup(balance);
+        uint256 incentive = balance * callIncentive / ONE;
         _mint(msg.sender, incentive);
+        IBoard(board).extendLockup(balance);
     }
 
     ////////////////////////////////////////////////////////////////
     //////////////////////////// Owner /////////////////////////////
     ////////////////////////////////////////////////////////////////
 
-    /// @notice update the board address, one time call
-    /// @param _board address of the new board
+    /// @notice updates the Board address, one time call
+    /// @param _board address of the Board contract
     function setBoard(address _board, uint256 _callIncentive) external onlyOwner {
         if (board != address(0)) revert BoardAlreadySet();
         if (!IBoard(_board).isBoard()) revert InvalidBoard();
         if (_callIncentive == 0 || _callIncentive > maxIncentive) revert InvalidIncentiveValue(_callIncentive);
         board = _board;
         callIncentive = _callIncentive;
-        mav.safeApprove(_board, type(uint256).max);
-        emit BoardSet(_board);
+        boardSetAt = block.timestamp;
+        emit BoardSet(_board, _callIncentive);
     }
 
-    /// @notice updates the incentive rate for calling lock limiting it to 1%
+    /// @notice updates the incentive rate for extending the lock
     /// @param _callIncentive new incentive rate
     function updateIncentive(uint256 _callIncentive) external onlyOwner {
         if (_callIncentive == 0 || _callIncentive > maxIncentive) revert InvalidIncentiveValue(_callIncentive);
@@ -115,7 +105,10 @@ contract Locker is OFT {
 
     /// @notice disable withdrawals
     function disable() external onlyOwner {
+        if (disabled || boardSetAt == 0) revert InvalidDisabling();
+        if (block.timestamp < boardSetAt + 3 days) revert TimelockPeriodNotPassed();
         disabled = true;
+        mav.approve(board, type(uint256).max);
         emit WithdrawalsDisabled();
     }
 }
