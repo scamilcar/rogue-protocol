02af8c7beb3da8f12076e72e72bfa6c8f9c66341
scamilcar <raclimacs@gmail.com>
2023-11-08 17:41:21 +0700

    Adapt boosters, test

--- contracts/core/Booster.sol
+++ contracts/core/Booster.sol
@@ -1,17 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
-import {ERC20,IERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
+import "@ERC4626/src/xERC4626.sol";
 
 import {IReward} from "@maverick/interfaces/IReward.sol";
 import {IPoolPositionSlim} from "@maverick/interfaces/IPoolPositionSlim.sol";
 
 import {Rewarder} from "contracts/core/base/Rewarder.sol";
-import {IManager} from "contracts/core/interfaces/IManager.sol";
-import {IHub} from "contracts/periphery/interfaces/IHub.sol";
 import {IBoard} from "contracts/periphery/interfaces/IBoard.sol";
 
 /*
@@ -19,10 +14,9 @@ TODO: mint oROG nft when claiming optionToken
 TODO: events
 */
 
-contract Booster is ERC4626, Rewarder {
-    using SafeERC20 for IERC20;
-
-    error EOA();
+contract Booster is xERC4626, Rewarder {
+    using SafeTransferLib for ERC20;
+    using SafeCastLib for *; 
 
     /// @notice the address of the broker
     address public immutable broker;
@@ -49,30 +43,21 @@ contract Booster is ERC4626, Rewarder {
         string memory name_,
         string memory symbol_)
 
-        ERC4626(IERC20(_poolPosition))
-        Rewarder(_manager)
-        ERC20(name_, symbol_) { 
+        ERC4626(ERC20(_poolPosition), name_, symbol_)
+        xERC4626(7 days)
+        Rewarder(_manager) { 
 
         lpReward = _lpReward;
         broker = _broker;
         board = _board;
 
-        IERC20(_poolPosition).approve(lpReward, type(uint256).max);
+        ERC20(_poolPosition).approve(lpReward, type(uint256).max);
     }
 
-    ////////////////////////////////////////////////////////////////
-    /////////////////////////// View ////////////////////////////
-    ////////////////////////////////////////////////////////////////
-
-    /// @notice overriden ERC4626 method
-    function totalAssets() public view override returns (uint256) {
-        return IReward(lpReward).balanceOf(board);
-    }
-
-    ////////////////////////////////////////////////////////////////
-    ///////////////////////// User-facing //////////////////////////
-    ////////////////////////////////////////////////////////////////
-
+    /*//////////////////////////////////////////////////////////////
+                             REWARD LOGIC
+    //////////////////////////////////////////////////////////////*/
+    
     /// @notice get caller's rewards for a list of reward token index
     /// @param recipient address to receive the rewards
     /// @param rewardTokenIndices list of reward token index
@@ -88,55 +73,135 @@ contract Booster is ERC4626, Rewarder {
         return _getReward(msg.sender, recipient, rewardTokenIndex);
     }
     
-    ////////////////////////////////////////////////////////////////
-    /////////////////////////// Override ///////////////////////////
-    ////////////////////////////////////////////////////////////////
+    /*//////////////////////////////////////////////////////////////
+                        DEPOSIT/WITHDRAWAL LOGIC
+    //////////////////////////////////////////////////////////////*/
 
-    /// @notice ERC4626 modified internal function. It stakes the asset in lpReward with board as recipient
-    function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal override {
-        // slither-disable-next-line reentrancy-no-eth
-        IERC20(asset()).safeTransferFrom(caller, address(this), assets);
+    function deposit(uint256 assets, address receiver) public override returns (uint256 shares) {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
         IReward(lpReward).stake(assets, board);
         _stake(shares, receiver);
+
         _mint(receiver, shares);
 
-        emit Deposit(caller, receiver, assets, shares);
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
     }
 
-    /// @notice ERC4626 modified internal function. It calls the Board to unstake assets which sends them to receiver
-    function _withdraw(
-        address caller,
-        address receiver,
-        address owner,
+    function mint(uint256 shares, address receiver) public override returns (uint256 assets) {
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        asset.safeTransferFrom(msg.sender, address(this), assets);
+
+        IReward(lpReward).stake(assets, board);
+        _stake(shares, receiver);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function withdraw(
         uint256 assets,
-        uint256 shares
-    ) internal override {
-        if (caller != owner) {
-            _spendAllowance(owner, caller, shares);
+        address receiver,
+        address owner
+    ) public override returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
         }
+
+        beforeWithdraw(assets, shares);
+
         _burn(owner, shares);
+
         _unstake(shares, owner);
         IBoard(board).unstake(lpReward, assets, receiver);
 
-        emit Withdraw(caller, receiver, owner, assets, shares);
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
     }
 
-    /// @notice update rewards before transfer
-    /// @param from address to transfer from
-    /// @param to address to transfer to
-    /// @param amount amount to transfer
-    /// @dev would not update rewards, stakes and  if `to` is whitelisted
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        if (from != address(0) && to != address(0)) {
-            updateAllRewards(from);
-            updateAllRewards(to);
-            _updateStakes(from, to, amount);
-        } 
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public override returns (uint256 assets) {
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        _unstake(shares, owner);
+        IBoard(board).unstake(lpReward, assets, receiver);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
     }
 
-    /// @notice updates stakes
-    function _updateStakes(address from, address to, uint256 amount) internal {
-        stakeOf[from] -= amount;
-        stakeOf[to] += amount;
+    /*//////////////////////////////////////////////////////////////
+                             TRANSFER LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        super.transfer(to, amount);
+        _unstake(amount, msg.sender);
+        _stake(amount, to);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override returns (bool){
+        super.transferFrom(from, to, amount);
+        _unstake(amount, from);
+        _stake(amount, to);
+        return true;
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             xERC4626 LOGIC
+    //////////////////////////////////////////////////////////////*/
+
+    function syncRewards() public override {
+        uint192 lastRewardAmount_ = lastRewardAmount;
+        uint32 timestamp = block.timestamp.safeCastTo32();
+
+        if (timestamp < rewardsCycleEnd) revert SyncError();
+
+        uint256 storedTotalAssets_ = storedTotalAssets;
+        /// @dev Board has the asset balance
+        uint256 nextRewards = asset.balanceOf(board) - storedTotalAssets_ - lastRewardAmount_;
+
+        storedTotalAssets = storedTotalAssets_ + lastRewardAmount_; // SSTORE
+
+        uint32 end = ((timestamp + rewardsCycleLength) / rewardsCycleLength) * rewardsCycleLength;
+
+        // Combined single SSTORE
+        lastRewardAmount = nextRewards.safeCastTo192();
+        lastSync = timestamp;
+        rewardsCycleEnd = end;
+
+        emit NewRewardsCycle(end, nextRewards);
     }
 }
